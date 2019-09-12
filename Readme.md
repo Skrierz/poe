9a6f24f5f2ad96ad156b70b51ebd9b3804555cf9
adenisyuk <A.Denisyuk@aladdin-rd.ru>
2019-09-12 19:41:55 +0300

    feat: Code editing
    Added receiving character data from pathofexile.com
    Added docs
    Refactor code structure

--- Readme.md
+++ Readme.md
@@ -1,4 +1,5 @@
 #### Usefull links:
+* My char: https://www.pathofexile.com/character-window/get-items?character=BlyatLiga
 * Api refs: pathofexile.com/forum/view-thread/1212138/page/1
 * POE devs subreddit: https://www.reddit.com/r/pathofexiledev/comments/3vzmwv/overview/
 * PyPOE docs: http://omegak2.net/poe/PyPoE/
--- test.py
+++ test.py
@@ -3,12 +3,32 @@ import json
 import browser_cookie3 # todo: check this lib and look for alternatives
 
 
-def get_data():
-    with open('response.txt', 'r') as fp:
-        data = json.load(fp)
+def get_character_data(character_name):
+    '''
+    Get character data from server.
+    You can access ONLY your character.
+    And also you need to be logged in 'www.pathofexile.com'
+
+    :param character_name: Character name which data you want to receive
+    :return: Data in json format
+    '''
+    character_data_url = f'https://www.pathofexile.com/character-window/get-items?character={character_name}'
+
+    # Get cookies from all browsers
+    cj = browser_cookie3.load()
+
+    return requests.get(character_data_url, cookies=cj).json()
 
 
-def get_equiped_gems_requiremens():
+def get_equipped_gems_requirements(data):
+    '''
+    Parse character data and return info about equipped gems and their requirements
+
+    :param data: Character data in json
+    :return: Dict with key = gem name and value = list of tuples, where: 1st item - stat name; 2nd item - needed value
+    '''
+    gems = {}
+
     for item in data['items']:
         try:
             item['socketedItems']
@@ -19,16 +39,15 @@ def get_equiped_gems_requiremens():
             for req in gem['requirements']:
                 if req['name'] in ('Dex', 'Int', 'Str'):
                                  requirements.append((req['name'], req['values'][0][0]))
-            print(f'gem: {gem["typeLine"]}. requiremens: {requirements}')
+            gems[gem["typeLine"]] = requirements
 
-
-def test():
-    cj = browser_cookie3.chrome()
-    return requests.get('https://www.pathofexile.com', cookies=cj)
+    return gems
 
 
 def main():
-    print(test())
+    CHARACTER = 'BlyatLiga'
+    gems = get_equipped_gems_requirements(get_character_data(CHARACTER))
+    [print(f'gem: {k}. requirements: {v}') for k,v in gems.items()]
 
 
 if __name__ == '__main__':

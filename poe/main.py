5468ee767cfeaa16056f4910caba397fc1ed90f4
Skrierz <sleipknir@gmail.com>
2019-10-03 01:41:52 +0300

    fix: Change user input

--- poe/main.py
+++ poe/main.py
@@ -2,9 +2,11 @@ from poe.core.core import get_character_data, get_equipped_gems_requirements
 
 
 def main():
+    account_name = input('Введите имя Вашего аккаунта: ')
+    realm = input('Введите ваш реалм ("pc", "ps4"(?), "xbox"(?)): ')
     character_name = input('Введите имя Вашего персонажа: ')
 
-    gems = get_equipped_gems_requirements(get_character_data(character_name))
+    gems = get_equipped_gems_requirements(get_character_data(account_name, realm, character_name))
     [print(f'gem: {x["name"]}. requirements: {x["requirements"]}') for x in gems]
 
     input('Press Enter to close app...')
